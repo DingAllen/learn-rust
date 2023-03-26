db599c457f669ba2cd5f33784f8dc27164386044
DingAllen <1121174884@qq.com>
2023-03-27 07:56:22 +0800

    HelloWorld

---
+++ src/hello_world.rs
@@ -0,0 +1,8 @@
+pub fn hello_world() {
+    println!("Hello, world!");
+    println!("I'm a Rustacean!");
+}
+
+pub fn say_hello(name: &str) {
+    println!("Hello, {}!", name);
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod hello_world;
+
+fn main() {
+    hello_world::hello_world();
+    hello_world::say_hello("Rustacean");
+}
